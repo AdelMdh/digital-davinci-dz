6508987f5924f680ce25e4a3991b5623bc530d0c
AdelMdh <91626632+AdelMdh@users.noreply.github.com>
2022-04-26 03:41:39 +0100

    premier changement dans la nav barr

---
+++ main/script.js
@@ -0,0 +1,8 @@
+var observer = new IntersectionObserver(function(entries) {
+	// isIntersecting is true when element and viewport are overlapping
+	// isIntersecting is false when element and viewport don't overlap
+	if(entries[0].isIntersecting === false)
+		console.log('Element has just become visible in screen');
+}, { threshold: [0] });
+
+observer.observe(document.querySelector("#main-container"));
