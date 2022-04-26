80ecf8f4a90042724a5bd6216d418f058f22c767
AdelMdh <91626632+AdelMdh@users.noreply.github.com>
2022-04-26 06:29:39 +0100

    navbarr: premier js

--- main/script.js
+++ main/script.js
@@ -1,8 +1,16 @@
-var observer = new IntersectionObserver(function(entries) {
+
+
+const nav = document.querySelector(".navigation");
+const navText = document.querySelectorAll("#navText")
+
+console.log(nav.className)
+let observer = new IntersectionObserver(function(entries) {
 	// isIntersecting is true when element and viewport are overlapping
 	// isIntersecting is false when element and viewport don't overlap
 	if(entries[0].isIntersecting === false)
-		console.log('Element has just become visible in screen');
+		console.log(nav.className)
+		nav.classList.toggle("navigationBlanc");
 }, { threshold: [0] });
 
 observer.observe(document.querySelector("#main-container"));
+
